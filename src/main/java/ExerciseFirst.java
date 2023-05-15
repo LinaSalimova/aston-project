f55ef6cec949415be8a05d36322b76c177436299
LinaAlekseeva <106020410+LinaAlekseeva@users.noreply.github.com>
2023-05-15 17:53:33 +0300

    Exercise first and second

--- src/main/java/ExerciseFirst.java
+++ src/main/java/ExerciseFirst.java
@@ -4,6 +4,7 @@ public class ExerciseFirst {
     //если введенное число больше 7, то вывести “Привет”
     public static void main(String[] args) {
         Scanner num = new Scanner(System.in);
+        System.out.println("Введите число:");
         int i;
         i = num.nextInt();
         if (i > 7) {
